93ed063683ff5d502a0f452f99521327bbfc69eb
BiancaCassel <162519574+BiancaCassel@users.noreply.github.com>
2024-03-14 10:24:06 -0300

    Add files via upload

---
+++ lista1/exercício3.py
@@ -0,0 +1,18 @@
+# Imprime a questão e as alternativas na tela
+print("Qual é o verdadeiro nome do super-herói Batman?")
+print("a) Bruce Wayne")
+print("b) Clark Kent")
+print("c) Peter Parker")
+print("d) Tony Stark")
+print("e) Steve Rogers")
+
+# Armazena a resposta correta em uma variável
+resposta_correta = 'a'
+
+# Solicita ao usuário que digite sua resposta e armazena em uma variável
+resposta_usuario = input("Digite sua resposta (letra correspondente): ")
+
+# Exibe a resposta do usuário e a resposta correta
+print("Você respondeu alternativa", resposta_usuario )
+print("A resposta correta é a alternativa", resposta_correta )
+
---
+++ lista1/exercício4.py
@@ -0,0 +1,14 @@
+print("Qual é o verdadeiro nome do super-herói Batman?")
+print("a) Bruce Wayne")
+print("b) Clark Kent")
+print("c) Peter Parker")
+print("d) Tony Stark")
+print("e) Steve Rogers")
+
+resposta_usuario = input("Digite sua resposta (letra correspondente): ")
+
+if resposta_usuario == 'a':
+    print("Você acertou!")
+else:
+    print("Você errou, a resposta correta era a alternativa 'a'.")
+
