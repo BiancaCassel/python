b7cf53a577df781df1800713e29afaa1c6eb752a
BiancaCassel <162519574+BiancaCassel@users.noreply.github.com>
2024-03-14 10:31:16 -0300

    Update ex1.py

--- lista2/ex1.py
+++ lista2/ex1.py
@@ -1,1 +1,13 @@
+def converter_minutos_para_segundos():
+    # Ler a quantidade de minutos
+    minutos = int(input("Digite a quantidade de minutos: "))
+    
+    # Calcular o tempo em segundos
+    segundos = minutos * 60
+    
+    # Escrever o resultado na tela
+    print("O tempo equivalente em segundos é:", segundos)
+
+# Chamar a função para testar
+converter_minutos_para_segundos()
 
