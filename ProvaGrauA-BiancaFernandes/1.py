f2fa2d9fec6eab435bdb6d306cd70941017f4a53
BiancaCassel <162519574+BiancaCassel@users.noreply.github.com>
2024-04-24 21:03:14 -0300

    Create 1.py

---
+++ ProvaGrauA-BiancaFernandes/1.py
@@ -0,0 +1,9 @@
+def divisivelPor2(numero):
+    if numero % 2 == 0:
+        return True
+    else:
+        return False
+int(input("digite um numero: "))
+
+numero = 10
+print(divisivelPor2(numero))  
