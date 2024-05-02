5cd9fde68a05cbd74abdd1fc882ffe54eb07c2b0
BiancaCassel <162519574+BiancaCassel@users.noreply.github.com>
2024-05-01 23:13:46 -0300

    Create ex1c.py

---
+++ lista4/ex1c.py
@@ -0,0 +1,10 @@
+print("Usando for loop:")
+for numero in range(70, 24, -1):  # começa em 70, termina em 25, decremento de 1
+    print(numero)
+
+
+print("\nUsando while loop:")
+numero = 70
+while numero >= 25:
+    print(numero)
+    numero -= 1
