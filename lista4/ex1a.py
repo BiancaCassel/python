c969965e56f7de2af4016bab78a9e02729493f77
BiancaCassel <162519574+BiancaCassel@users.noreply.github.com>
2024-05-01 23:10:50 -0300

    Create ex1a.py

---
+++ lista4/ex1a.py
@@ -0,0 +1,11 @@
+# Usando while loop
+print("Usando while loop:")
+numero = 0
+while numero <= 100:
+    print(numero)
+    numero += 1
+
+# Usando for loop
+print("\nUsando for loop:")
+for numero in range(101):
+    print(numero)
