942d3a147c24ba71f0407b1c2826a681149761e1
BiancaCassel <162519574+BiancaCassel@users.noreply.github.com>
2024-05-01 23:35:14 -0300

    Create ex3.py

---
+++ lidts5/ex3.py
@@ -0,0 +1,7 @@
+def mediaUnisinos(nota_a, nota_b):
+    media_final = (nota_a + 2 * nota_b) / 2
+    return media_final
+
+# Exemplo de uso da função
+resultado_final = mediaUnisinos(7.5, 8.0)
+print("Resultado do grau final:", resultado_final)
